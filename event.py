1b293065a3ae4049b446c7d9c0f74868fb2f3d44
ajagdish <aniboyrules@gmail.com>
2017-02-23 19:00:14 -0800

    Delete event.py

--- event.py
+++
@@ -1,9 +0,0 @@
-class Event(object):
-    def __init__(self, eTime, servTime, eType):
-        self.eTime = eTime
-        self.servTime = servTime
-        self.eType = eType
-        self.print_details()
-
-    def print_details(self):
-        print ("Event: ", self.eType, "\tTime: ", self.eTime, "\tService Time: ", self.servTime)
